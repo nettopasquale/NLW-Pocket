214fad00604f408678377ab7ec449d39d387a819
Pasquale <pasqualenetto@yahoo.com.br>
2024-09-10 18:58:29 -0300

    Inicia menu do app

--- aula.md
+++ aula.md
@@ -5,13 +5,11 @@ Esse é um markdown.
 **Algoritmos**:
 
 ## Peças de uma linguagem
-
 -
 -
 -
 -
 -
-
 
 ## Fases da resolução de um problema
 -
@@ -20,8 +18,29 @@ Esse é um markdown.
 -
 -
 
-
 ## Tipos de dado
 
-
 # Operadores
+
+# Await / async
+
+- Basicamente, é uma função assíncrona que depende do término de outra função para acontecer
+
+- Inicialmente tinhamos uma função que, ao ser chamada, iniciava um switch/case a partir da referência "opciones";
+- como essa referência já tinha um dado bruto, ela sempre retorna o valor dentro do esperado;
+
+- Mas para deixar melhor essa função, queremos que esse switch/case dependa do prompt do usuário.
+
+- A melhor forma para isso é tornar a função assíncrona, pois assim a refência "opciones" só terá um valor atribuido quando
+- o prompt do usuário captar um input; Para esse input vamos usar a biblioteca "Inquirer"
+
+- Ou seja, "opciones" precisa ESPERAR que a função SELECT de INQUIRER seja resolvida primeiro;
+
+
+
+## Promises
+
+- Await e async são fundamentais para entender Promises (literalmente promessas)
+- isso porque, a resposta da função que recebe "await" é chamada de Promise
+
+- No caso acima, se "opciones" não tivesse um await, ou seja, uma promessa de uma resposta, ela n teria atribuição nenhuma
--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-const start = () => {
-    let opcionies = "";
+const { select } = require('@inquirer/prompts');
 
-    switch (opcionies) {
-        case "cadastrar":
-            console.log("Lets cadastr");
-            break;
-        case "listar":
-            console.log("vamos listar");
-            break;
-        default:
-            console.error("Opção inválida :(");
+const start = async () => {
+
+    // Que situação aqui seria falso?
+    while (true) {
+            // select tem que ser resolvida antes de ser atribuida em opciones
+            const opcionies =  await select({
+                message: "Menu >", // mensagem simples
+                choices: [ // escolhas de input, ARRAY!
+                    {
+                        name: "Cadastra meta", //mensagem 1
+                        value: "cadastrar" // valor da mensagem 1
+                    },
+                    {
+                        name: "Listar metas", 
+                        value: "listar" 
+                    },
+                    {
+                        name: "Sair",
+                        value: "sair"
+                    }
+                ]
+            });
+        
+            switch (opcionies) {
+                case "cadastrar":
+                    console.log("Lets cadastrar");
+                    break;
+                case "listar":
+                    console.log("vamos listar");
+                    break;
+                case "sair":
+                    return;
+                
+            }
+        }
     }
-}
+
 
 start()
