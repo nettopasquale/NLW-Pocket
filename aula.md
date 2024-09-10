8fd2e02bb49db868bba02dd8e8b1283f9ee10091
Pasquale <pasqualenetto@yahoo.com.br>
2024-09-10 16:46:55 -0300

    primeiro commit, inicio projeto

---
+++ aula.md
@@ -0,0 +1,27 @@
+## LP
+
+Esse é um markdown.
+
+**Algoritmos**:
+
+## Peças de uma linguagem
+
+-
+-
+-
+-
+-
+
+
+## Fases da resolução de um problema
+-
+-
+-
+-
+-
+
+
+## Tipos de dado
+
+
+# Operadores
---
+++ index.js
@@ -0,0 +1,10 @@
+let meta = {
+    value: 'Codar uma coisa por mês',
+    checked: false,
+    //imprime o argumento no console
+    log: (info) => {
+        console.log(info);
+    }
+}
+
+meta.log(meta.value);
